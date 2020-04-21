9c45004b95193da79c173b5f150c87a838b52650
djeffersonsilva <djeffersonsilva@vhsolucoes.com.br>
2020-04-21 17:40:51 -0300

    ...

--- news_reader/spiders/text_analyzer.py
+++ news_reader/spiders/text_analyzer.py
@@ -37,13 +37,7 @@ def getTextAnalysis(text):
     return 'ntr'
 
 a = """
-(Reuters) - U.S. car rental company Hertz Global Holdings Inc (N:HTZ) said on Monday it plans to lay off 10,000 employees across its North America operations to cut costs amid the economic fallout of the COVID-19 pandemic.
-Hertz had about 38,000 employees as of Dec. 31, 2019, of which 29,000 were at its U.S. operations.
-The company, which counts billionaire investor Carl Icahn as its largest shareholder, will incur employee termination costs of about $30 million, it said in a regulatory filing. (https://
-The terminations were effective April 14 for non-union employees and effective April 21 for union employees, the company said.
-Late last month, Hertz said it was implementing furlough programs across its North America operations to cope with slowing demand for its services. (https://
-"Like the rest of the global travel sector, COVID-19's impact on Hertz arrived swiftly, and the reversal in customer demand has been significant," Chief Executive Officer Kathryn Marinello said in March.
-The company also said last month it hoped to bring back as many team members as possible once global travel rebounds.
+demission
 """
 
 print(getTextAnalysis(a))
